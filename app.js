a91d5f26aad78d2681d973d808faf42a2b9dd939
Romain <RomainFrechic@outlook.fr>
2016-10-03 16:40:13 +0200

    quatrequartclicker

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	//votre code
+	var compteur = 0;
+$("#img").click(function(){
+$("span").text(++compteur);
 
+if(compteur % 20 === 0){
+	alert("vous êtes a la 20eme valeur ")
+}
+
+});
 
 });
